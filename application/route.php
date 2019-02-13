46c45fec63eebfff0afa2c90092a9400a10dc0c6
huipd <13990839247@163.com>
2019-02-13 18:26:24 +0800

    修改

---
+++ application/route.php
@@ -0,0 +1,13 @@
+<?php
+// +----------------------------------------------------------------------
+// | 强制路由管理
+// +----------------------------------------------------------------------
+use think\Route
+// 加载前台首页
+Route::get('/','index/Index/getindex');
+// 登陆注册
+Route::controller('/pass','index/index');
+// 加载后台登陆模板
+Route::controller('/adminlogin','admin/Login');
+Route::controller('/admin','admin/Index');
+Route::controller('/admins','admin/Admins');
